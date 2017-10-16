17de8db87d9eb610d7ea171e09a1a429a9a9b381
David Mrabu <davidjblaze@hotmail.com>
2017-10-16 14:16:22 +0300

    Added multiply to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,21 @@ var add = function(number1, number2) {
       var result = add(number1, number2);
       alert(result);
 //subtraction is below
-var subtraction = function(number1, number2) {
+var subtract = function(number1, number2) {
         return number1 - number2;
       };
 
       var number1 = parseInt(prompt("Enter a number:"));
       var number2 = parseInt(prompt("Enter another number:"));
-      var result = subtraction(number1, number2);
+      var result = subtract(number1, number2);
+      alert(result)
+//multiply is below
+var multiply = function(number1, number2) {
+        return number1 * number2;
+      };
+
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another:"));
+      var result = multiply(number1, number2);
       alert(result);
+      
