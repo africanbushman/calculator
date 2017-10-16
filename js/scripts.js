58afcdfa298282b69e521042880c45bd141ef09e
David Mrabu <davidjblaze@hotmail.com>
2017-10-16 14:03:51 +0300

    Added sutraction to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,12 @@ var add = function(number1, number2) {
       var number2 = parseInt(prompt("Enter another number:"));
       var result = add(number1, number2);
       alert(result);
+//subtraction is below
+var subtraction = function(number1, number2) {
+        return number1 - number2;
+      };
+
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another number:"));
+      var result = subtraction(number1, number2);
+      alert(result);
