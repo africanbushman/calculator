ca83ddadec5885229677e2bbdbdfab43cd15544c
David Mrabu <davidjblaze@hotmail.com>
2017-10-16 14:20:37 +0300

    Added fix to divide to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -26,7 +26,7 @@ var multiply = function(number1, number2) {
       alert(result);
 // divide is below
 var divide = function(number1, number2) {
-        return number1 * number2;
+        return number1 / number2;
       };
 
       var number1 = parseInt(prompt("Enter a number:"));
