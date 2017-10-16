9be9b49692d4967be882451ab3799148bc8e3c70
David Mrabu <davidjblaze@hotmail.com>
2017-10-16 14:18:18 +0300

    Added divide to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,4 +24,12 @@ var multiply = function(number1, number2) {
       var number2 = parseInt(prompt("Enter another:"));
       var result = multiply(number1, number2);
       alert(result);
-      
+// divide is below
+var divide = function(number1, number2) {
+        return number1 * number2;
+      };
+
+      var number1 = parseInt(prompt("Enter a number:"));
+      var number2 = parseInt(prompt("Enter another:"));
+      var result = divide(number1, number2);
+      alert(result);
