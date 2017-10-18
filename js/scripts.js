feb9cb04da19663e66da8f1698e7b1847627ed55
David Mrabu <davidjblaze@hotmail.com>
2017-10-18 11:48:54 +0300

    Changed the javascript to show backend and front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,26 @@
-//addition is below
-var add = function(number1, number2) {
-        return number1 + number2;
-      };
+// Business (or back-end) logic:
 
-      var number1 = parseInt(prompt("Enter a number:"));
-      var number2 = parseInt(prompt("Enter another number:"));
-      var result = add(number1, number2);
-      alert(result);
-//subtraction is below
-var subtract = function(number1, number2) {
-        return number1 - number2;
-      };
+     var add = function(number1, number2) {
+       return number1 + number2;
+     };
 
-      var number1 = parseInt(prompt("Enter a number:"));
-      var number2 = parseInt(prompt("Enter another number:"));
-      var result = subtract(number1, number2);
-      alert(result)
-//multiply is below
-var multiply = function(number1, number2) {
-        return number1 * number2;
-      };
+     var subtract = function(number1, number2) {
+       return number1 - number2;
+     };
 
-      var number1 = parseInt(prompt("Enter a number:"));
-      var number2 = parseInt(prompt("Enter another number:"));
-      var result = multiply(number1, number2);
-      alert(result);
-// divide is below
-var divide = function(number1, number2) {
-        return number1 / number2;
-      };
+     var multiply = function(number1, number2) {
+       return number1 * number2;
+     };
 
-      var number1 = parseInt(prompt("Enter a number:"));
-      var number2 = parseInt(prompt("Enter another number:"));
-      var result = divide(number1, number2);
-      alert(result);
+     var divide = function(number1, number2) {
+       return number1 / number2;
+     };
+
+     // Everything below this line is user interface (or front-end) logic:
+
+     $(document).ready(function() {
+       var number1 = parseInt(prompt("Enter a number:"));
+       var number2 = parseInt(prompt("Enter another number:"));
+
+       alert(add(number1, number2));
+     });
